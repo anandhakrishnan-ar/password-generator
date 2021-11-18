874636748a110370723f76fe655e128cdb246aaa
anandhakrishnan-ar <anandhank77@gmail.com>
2021-11-18 15:28:12 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="app-container row">
+            <div className="col-md-12">
+            <h1 className="h1">Strong Password Generator</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
